5b7c66f87a9b779b4141be6fe4eb1eeffdd5c420
Dhruv Maheshwari <mrdm026@gmail.com>
2022-01-08 18:05:22 +0530

    Challenge-01 Print out our name

---
+++ Part-01/src/Challenges/Challenge_01.java
@@ -0,0 +1,8 @@
+package Challenges;
+
+// Printing out our name
+public class Challenge_01 {
+    public static void main(String[] args) {
+        System.out.println("Hello, Dhruv Maheshwari");
+    }
+}
