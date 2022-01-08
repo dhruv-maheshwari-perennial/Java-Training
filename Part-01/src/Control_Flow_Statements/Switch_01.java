f72aebc7eccd490af9b8959ff5b3565cc5bc6e8b
Dhruv Maheshwari <mrdm026@gmail.com>
2022-01-09 02:36:50 +0530

    Created Switch file

---
+++ Part-01/src/Control_Flow_Statements/Switch_01.java
@@ -0,0 +1,19 @@
+package Control_Flow_Statements;
+
+public class Switch_01 {
+    public static void main(String[] args) {
+        int value = 1;
+
+        switch (value) {
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            default:
+                System.out.println("Wrong Value");
+
+        }
+    }
+}
