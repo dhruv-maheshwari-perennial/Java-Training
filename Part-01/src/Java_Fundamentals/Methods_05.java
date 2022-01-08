04994cc903e481fcac3fb53ba65e6018184cc7f5
Dhruv Maheshwari <mrdm026@gmail.com>
2022-01-09 02:15:09 +0530

    Added Method Overloading Example

--- Part-01/src/Java_Fundamentals/Methods_05.java
+++ Part-01/src/Java_Fundamentals/Methods_05.java
@@ -1,34 +1,48 @@
 package Java_Fundamentals;
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class Methods_05 {
     public static void main(String[] args) {
         // for taking input from user
-        Scanner sc = new Scanner(System.in);
+//        Scanner sc = new Scanner(System.in);
 
-        int bonus = 100;
-        int score, levelCompleted;
+//        int bonus = 100;
+//        int score, levelCompleted;
 
-        System.out.print("Enter your score: ");
-        score = sc.nextInt();
-        System.out.print("Enter no. of levels completed: ");
-        levelCompleted = sc.nextInt();
+//        System.out.print("Enter your score: ");
+//        score = sc.nextInt();
+//        System.out.print("Enter no. of levels completed: ");
+//        levelCompleted = sc.nextInt();
+//
+//        int finalScore = calculateScore(true, score, levelCompleted, bonus);
+//        System.out.println("Total Score: " + finalScore);
 
-        int finalScore = calculateScore(true, score, levelCompleted, bonus);
-        System.out.println("Total Score: " + finalScore);
+        person("Dhruv Maheshwari");
+        person(21);
+        person(20000.0);
+
     }
 
     // method with return statement
-    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
-        int totalScore = 0;
-        if (gameOver) {
-            totalScore = score + (levelCompleted * bonus);
-//            System.out.println("Total Score: " + totalScore);
-        }
-//        else {
-//            System.out.println("Game not finished yet.");
+//    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+//        int totalScore = 0;
+//        if (gameOver) {
+//            totalScore = score + (levelCompleted * bonus);
 //        }
-        return totalScore;
+//        return totalScore;
+//    }
+
+    //method overloading
+    public static void person(String name){
+        System.out.println("Name: " + name);
+    }
+
+    public static void person(int age){
+        System.out.println("Age: " + age);
+    }
+
+    public static void person(double salary){
+        System.out.println("Salary: " + salary);
     }
 }
