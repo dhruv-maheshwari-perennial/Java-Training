ed19de6da7cfba6ea11ee4037c3efab090696d6c
Dhruv Maheshwari <mrdm026@gmail.com>
2022-01-08 17:45:15 +0530

    Add comments in HelloWorld.java

--- Part-01/src/HelloWorld.java
+++ Part-01/src/HelloWorld.java
@@ -1,5 +1,10 @@
+// class - HelloWorld
 public class HelloWorld {
+
+    // main function - entry point of java code
     public static void main(String[] args) {
+
+        // print statement
         System.out.println("Hello");
     }
 }
