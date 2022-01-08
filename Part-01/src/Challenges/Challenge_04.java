1b51bfbb6ae356df9500863a52cd3e098a85be3d
Dhruv Maheshwari <mrdm026@gmail.com>
2022-01-08 19:30:51 +0530

    convert a given number of pounds to kilograms

---
+++ Part-01/src/Challenges/Challenge_04.java
@@ -0,0 +1,10 @@
+package Challenges;
+
+//convert a given number of pounds to kilograms
+public class Challenge_04 {
+    public static void main(String[] args) {
+        double pound = 12;
+        double kgResult = pound * 0.4535;
+        System.out.println(kgResult + "Kg");
+    }
+}
