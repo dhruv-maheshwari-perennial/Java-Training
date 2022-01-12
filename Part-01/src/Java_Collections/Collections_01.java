c1bce9957fb459beedbeb0b779b88b13bb0e4240
Dhruv Maheshwari <mrdm026@gmail.com>
2022-01-12 12:22:13 +0530

    Implemented Various List Methods - Java Collection Example file

--- Part-01/src/Java_Collections/Collections_01.java
+++ Part-01/src/Java_Collections/Collections_01.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 class Theatre {
     private final String theatreName;
-    private List<Seat> seats = new ArrayList<>();
+    public List<Seat> seats = new ArrayList<>();
 
     public Theatre(String theatreName, int numRows, int seatsPerRow) {
         this.theatreName = theatreName;
@@ -30,21 +30,6 @@ class Theatre {
             System.out.println("There is no seat " + seatNumber);
             return false;
         }
-
-//        for (Seat seat : seats) {
-//            System.out.println(".");
-//            if (seat.getSeatNumber().equals(seatNumber)) {
-//                requestedSeat = seat;
-//                break;
-//            }
-//        }
-//
-//        if (requestedSeat == null) {
-//            System.out.println("There is no seat " + seatNumber);
-//            return false;
-//        }
-//
-//        return requestedSeat.reserve();
     }
 
     public void getSeats() {
@@ -53,7 +38,7 @@ class Theatre {
         }
     }
 
-    private class Seat implements Comparable<Seat> {
+    public class Seat implements Comparable<Seat> {
         private final String seatNumber;
         private boolean reserved = false;
 
@@ -95,18 +80,82 @@ class Theatre {
 public class Collections_01 {
     public static void main(String[] args) {
         Theatre theatre = new Theatre("PVR", 8, 12);
-//        theatre.getSeats();
+        List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);
+        printList(seatCopy);
 
-        if (theatre.reserveSeat("H11")) {
-            System.out.println("Please Pay");
+        seatCopy.get(1).reserve();
+        if (theatre.reserveSeat("A02")) {
+            System.out.println("Please pay for A02");
         } else {
-            System.out.println("Sorry seat is taken");
+            System.out.println("Seat already reserved");
         }
 
-        if (theatre.reserveSeat("H11")) {
-            System.out.println("Please Pay");
-        } else {
-            System.out.println("Sorry seat is taken");
+        Collections.shuffle(seatCopy);
+        System.out.println("Printing Seat Copy");
+        printList(seatCopy);
+        System.out.println("Printing Theatre Seat Copy");
+        printList(theatre.seats);
+
+        Theatre.Seat minSeat = Collections.min(seatCopy);
+        Theatre.Seat maxSeat = Collections.max(seatCopy);
+
+        System.out.println("Min seat Number: " + minSeat.getSeatNumber());
+        System.out.println("Max seat Number: " + maxSeat.getSeatNumber());
+
+        sortList(seatCopy);
+        System.out.println("Printing Sorted Seat Copy");
+        printList(seatCopy);
+
+        List<Theatre.Seat> newList = new ArrayList<>(theatre.seats.size());
+        Collections.copy(newList, theatre.seats);
+    }
+
+    public static void printList(List<Theatre.Seat> list) {
+        for (Theatre.Seat seat : list) {
+            System.out.print(" " + seat.getSeatNumber());
+        }
+
+        System.out.println();
+        System.out.println("==========================");
+    }
+
+    public static void sortList(List<? extends Theatre.Seat> list) {
+        for (int i = 0; i < list.size() -1; i++) {
+            for (int j = i+1; j < list.size(); j++) {
+                if (list.get(i).compareTo(list.get(j)) > 0) {
+                    Collections.swap(list, i, j);
+                }
+            }
         }
     }
 }
+
+
+//        theatre.getSeats();
+//        if (theatre.reserveSeat("H11")) {
+//            System.out.println("Please Pay");
+//        } else {
+//            System.out.println("Sorry seat is taken");
+//        }
+//
+//        if (theatre.reserveSeat("H11")) {
+//            System.out.println("Please Pay");
+//        } else {
+//            System.out.println("Sorry seat is taken");
+//        }
+
+
+//        for (Seat seat : seats) {
+//            System.out.println(".");
+//            if (seat.getSeatNumber().equals(seatNumber)) {
+//                requestedSeat = seat;
+//                break;
+//            }
+//        }
+//
+//        if (requestedSeat == null) {
+//            System.out.println("There is no seat " + seatNumber);
+//            return false;
+//        }
+//
+//        return requestedSeat.reserve();
