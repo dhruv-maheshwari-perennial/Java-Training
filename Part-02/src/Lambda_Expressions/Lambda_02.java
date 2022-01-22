dbb7d663ac6a21c31b0da06d931675fe420127de
Dhruv Maheshwari <mrdm026@gmail.com>
2022-01-22 22:28:28 +0530

    Lambda Example 2 File - Functions

--- Part-02/src/Lambda_Expressions/Lambda_02.java
+++ Part-02/src/Lambda_Expressions/Lambda_02.java
@@ -3,6 +3,7 @@ package Lambda_Expressions;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.function.Function;
 import java.util.function.IntPredicate;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
@@ -35,12 +36,12 @@ class Employees {
 
 public class Lambda_02 {
     public static void main(String[] args) {
-        Employees kartik = new Employees("Kartik",30);
-        Employees mayank = new Employees("Mayank",50);
-        Employees dhruv = new Employees("Dhruv",52);
-        Employees pratham = new Employees("Pratham",42);
-        Employees harshit = new Employees("Harshit",35);
-        Employees ekagra = new Employees("Ekagra",22);
+        Employees kartik = new Employees("Kartik Maheshwari",30);
+        Employees mayank = new Employees("Mayank Varshney",50);
+        Employees dhruv = new Employees("Dhruv Maheshwari",52);
+        Employees pratham = new Employees("Pratham Yadav",42);
+        Employees harshit = new Employees("Harshit Kumar",35);
+        Employees ekagra = new Employees("Ekagra Kumar",22);
 
         List<Employees> emps = new ArrayList<Employees>();
         emps.add(kartik);
@@ -50,30 +51,29 @@ public class Lambda_02 {
         emps.add(harshit);
         emps.add(ekagra);
 
-        printEmployeesByAge(emps, "30", employees -> employees.getAge()>30);
-        printEmployeesByAge(emps, "30 and under", employees -> employees.getAge()<=30);
-        printEmployeesByAge(emps, "younger than 25", new Predicate<Employees>() {
-            @Override
-            public boolean test(Employees emps) {
-                return emps.getAge() < 25;
-            }
-        });
+        Function<Employees, String> getLastName = (Employees emp) -> {
+            return emp.getName().substring(emp.getName().indexOf(" ") + 1);
+        };
 
-        IntPredicate intp = i -> i > 15;
-        IntPredicate intp2 = i -> i < 100;
-        System.out.println(intp.test(10));
+        String lastName = getLastName.apply(emps.get(1));
+        System.out.println(lastName);
 
-        int a = 20;
-        System.out.println(intp.test(a+5));
-
-        System.out.println(intp.and(intp2).test(50));
-        System.out.println(intp.and(intp2).test(15));
+        Function<Employees, String> getFirstName = (Employees emp) -> {
+            return emp.getName().substring(0, emp.getName().indexOf(" "));
+        };
 
         Random random = new Random();
-        Supplier<Integer> randomSupplier = () -> random.nextInt(1000);
-        for (int i = 0; i < 10; i++) {
-            System.out.println(randomSupplier.get());
+        for (Employees emp: emps) {
+            if (random.nextBoolean()) {
+                System.out.println(getAName(getFirstName, emp));
+            } else {
+                System.out.println(getAName(getLastName, emp));
+            }
         }
+    }
+
+    private static String getAName(Function<Employees, String> getName, Employees emp) {
+        return getName.apply(emp);
     }
 
     private static void printEmployeesByAge(List<Employees> emps, String ageText, Predicate<Employees> ageCondition) {
