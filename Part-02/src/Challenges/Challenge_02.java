7dbd7f2e9df6f46bfe2248ddc75752f480006429
Dhruv Maheshwari <mrdm026@gmail.com>
2022-01-22 23:42:53 +0530

    All Challenges of Lambda Section

---
+++ Part-02/src/Challenges/Challenge_02.java
@@ -0,0 +1,55 @@
+package Challenges;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Challenge_02 {
+    public static void main(String[] args) {
+        // Challenge 9 & 10
+        List<String> topNames2015 = Arrays.asList(
+          "Amelia",
+          "Olivia",
+          "emily",
+          "Isla",
+          "Ava",
+          "oliver",
+          "Jack",
+          "Charlie",
+          "harry",
+          "Jacob"
+        );
+
+        List<String> firstUpperCaseList = new ArrayList<>();
+        topNames2015.forEach(name -> firstUpperCaseList.add(name.substring(0, 1).toUpperCase() + name.substring(1)));
+        firstUpperCaseList.sort(String::compareTo);
+        firstUpperCaseList.forEach(System.out::println);
+
+        System.out.println("--------------------");
+
+        // Challenge 11
+        firstUpperCaseList
+                .stream()
+                .map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
+                .sorted(String::compareTo)
+                .forEach(System.out::println);
+
+        // Challenge 12
+        long namesBegWithA = topNames2015
+                .stream()
+                .map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
+                .filter(name -> name.startsWith("A"))
+                .count();
+
+        System.out.println(namesBegWithA);
+
+        // Challenge 14
+        topNames2015
+                .stream()
+                .map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
+                .peek(System.out::println)
+                .sorted(String::compareTo)
+                .collect(Collectors.toList());
+    }
+}
